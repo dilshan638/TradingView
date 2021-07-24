6bb0fea297a168f1e84ec76198a185a7a26a2750
Kasun Sameera <kasun@persystance.com>
2021-07-24 18:51:28 +0000

    updated version

--- src/components/SignIn/SigninForm.vue
+++ src/components/SignIn/SigninForm.vue
@@ -18,7 +18,7 @@
                     <input type="password" placeholder="Password" v-model="state.password.password" class="form-control" /><br/>
                     <span class="error-msg" v-if="v$.password.password.$error">{{ v$.password.$errors[0].$message }} </span>
 
-                    <button class="centered" @click="login">Login</button>
+                    <button class="centered" @click="SubmitForm">Login</button>
                     <span class="forgot-link" @click="$refs.forgotpasswordmodal.openModal()">Forgot Password</span>
                     <span class="reg">to LDX eFolio?  <router-link to="/signup">Register here</router-link></span>
                 </div>
@@ -32,7 +32,7 @@
                 <template v-slot:body>
                     <h5 style="color:black">Enter your Email Address</h5>
                     <input type="email" class="form-control" placeholder="Email Address" style="color:#000" v-model="state.forgotpasswordemail" />
-                    <span class="error-msg" v-if="v$.forgotpasswordemail.$error">{{ v$.forgotpasswordemail.$errors[0].$message }} </span>
+                    <!-- <span class="error-msg" v-if="v$.forgotpasswordemail.$error">{{ v$.forgotpasswordemail.$errors[0].$message }} </span> -->
                 </template>
 
                 <template v-slot:footer>
@@ -52,7 +52,7 @@
                     <p>We have sent a 6 Digit code to <br/>
                     your registered email on LDCX</p>
 
-                    <b>ab***@***.com</b>
+                    <b>ab*@*.com</b>
 
                     <div class="form-group mb-4">
                         <input type="text" class="form-control" placeholder="Email Verification code" v-model="state.verificationCode" />
@@ -71,7 +71,7 @@
                 <template v-slot:footer>
                     <div>
                         <button @click="otpcheck">Next</button>
-                        <span class="resend-area">Didn't Received <a style="text-decoration: underline; color: blue; cursor: pointer;" @click="resend">Resend link</a></span>
+                        <span class="resend-area">Didn't Received <a href="#" @click="resend">Resend link</a></span>
                     </div>
                 </template>
             </modal>   
@@ -94,18 +94,17 @@
                     </div>
                 </template>
             </modal>
-            
     </div>
 </template>
 <script>
 import { Auth } from 'aws-amplify';
 import useValidate from '@vuelidate/core'
-import { required, email,sameAs } from '@vuelidate/validators'
+import { required, email } from '@vuelidate/validators'
 import { reactive, computed } from 'vue'
 import Modal from "../Modal/Modal.vue";
 
 export default {
-    name:'signinemail',
+    name:'signin',
     components: {
         Modal
     },
@@ -114,13 +113,7 @@ export default {
             email:'',
             password: {
                 password: ''
-            },
-            forgotpasswordemail:'',
-            verificationCode:'',
-            forgotPassword:{
-                    newPassword:''
-            },
-            confirmPassword:''
+            }
         })
 
         const rules = computed(() => {
@@ -133,24 +126,6 @@ export default {
                     password: { 
                         required
                     }
-                },
-                forgotpasswordemail:{
-                    email,
-                    required
-                },
-                forgotPassword:{
-                    newPassword:{
-                      required  
-                    },
-                     confirmPassword: { 
-                       required,
-                        sameAs:sameAs(state.forgotPassword.newPassword)
-                    }
-
-                },
-               verificationCode:{
-                    required
-                    
                 }
             }    
         }) 
@@ -163,11 +138,8 @@ export default {
             isHidden: false,
             showPassword: false,
             isHiddenMobile: false,
-            showPasswordMobile: false,
-            showForgotPasswordForm: false,
-            //forgotpasswordemail: '',
-           // verificationCode:'',
-           // newPassword:''
+            showPasswordMobile: false
+
         }
     },
     methods: {   
@@ -179,11 +151,6 @@ export default {
                     console.log(Cookies.set('accessToken', data.signInUserSession))
                     console.log(Cookies.set('firstName', data.attributes.name))
                     console.log(Cookies.set('lastName', data.attributes.middle_name))
-                    // console.log(Cookies.set('addressLine1', data.attributes.addressLine1))
-                    // console.log(Cookies.set('addressLine2', data.attributes.addressLine2))
-                    // console.log(Cookies.set('city', data.attributes.city))
-                     //console.log(Cookies.set('state', data.attributes.state))
-                    // console.log(Cookies.set('country', data.attributes.country))
                     this.accToken=data.signInUserSession.accessToken.jwtToken
                     this.role=true
                     console.log(this.accToken)
@@ -191,7 +158,6 @@ export default {
                     console.log(data.attributes.middle_name)
                 })
                     console.log('Yes')
-                   // this.$store.commit("setAuthentication",true);
                     this.$router.push('/kyc')
                      
                     
@@ -203,83 +169,10 @@ export default {
                     this.role=false
                 }
         },
-        async forgotpassword() {
-        // Send confirmation code to user's email
-         this.v$.forgotpasswordemail.$touch()
-          var username =this.state.forgotpasswordemail
 
-           if (!this.v$.$error){
-               await Auth.forgotPassword(username)
-                .then(data=>{
-                console.log(data)
-                console.log("Success")
-                this.$refs.forgotpasswordmodal.closeModal();
-                this.$refs.otpcodemodal.openModal();
-            
-            // this.$router.push('/ConfirmationCode');
-           })
-                .catch(err=>{
-                console.log(err)
-                console.log("Failed sending Code ")
-           })
-           }else{
-              console.log('Code Send Failed ')   
-           }
-            
-        },  
-        async otpcheck() {
-           this.v$.forgotpasswordemail.$touch()
-           this.v$.verificationCode.$touch()
-           this.v$.forgotPassword.newPassword.$touch()
-
-           if (!this.v$.$error) { // if ANY fail validation
-                var username =this.state.forgotpasswordemail
-                var code = this.state.verificationCode
-                var new_password = this.state.forgotPassword.newPassword
-               await  Auth.forgotPasswordSubmit(username, code, new_password )
-                .then(data=>{
-                console.log(data)
-                console.log("Success")
-                        })
-                .catch(err=>{
-                    console.log(err)
-                    console.log("Error")
-                })
-                this.$refs.otpcodemodal.closeModal();
-                this.$refs.successmodal.openModal();
-           } else {
-                console.log('Reset failed ')
-            }
-          //  this.$refs.resetpasswordmodal.openModal();
-
-        },
-        async resetpassword() {
-            this.$refs.resetpasswordmodal.closeModal();
-            this.$refs.resetpasswordmodal.openModal();
-        },
-
-        //Resend Verification Code
-
-        async resend(){
-             var username =this.state.forgotpasswordemail
-            await Auth.forgotPassword(username)
-            .then(data=>{
-            console.log(data)
-            console.log("Resend")
-
-           })
-            .catch(err=>{
-            console.log(err)
-            console.log(" Resend Error")
-           }) 
-            
-        },
         SubmitForm() {
-            
-           // this.v$.$validate()// checks all inputs
-           this.v$.email.$touch()
-           this.v$.password.$touch()
-            console.log( this.v$.email)
+            console.log('sucess')
+            this.v$.$validate() // checks all inputs
             if (!this.v$.$error) { // if ANY fail validation
                 this.login();
                 console.log('Form successfully submitted.')
