d6c21b95f2e3becec8c434ef52c2beaef4019afe
Kasun Sameera <kasun@persystance.com>
2021-07-27 09:56:00 +0000

    uodated changes

--- src/components/SignupForm/SignupForm.vue
+++ src/components/SignupForm/SignupForm.vue
@@ -31,44 +31,46 @@
                             v-bind:class="[profileTab ? 'show active' : '']"
                             id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                                 <div class="tab-view profile-view">
-                                    <div class="row">
-                                        <div class="col-md-5">
-                                            <div class="profile-pic">
-                                                <img :src="picture" class="uploading-image" />
-                                            </div>
-                                            <input type="file" accept="image/png, image/gif, image/jpeg" id="pro-pic" style="display:none" @change=uploadImage>
-                                            <label class="btn btn-primary dark centered" for="pro-pic">
-                                                Update Profile Picture
-                                            </label>                                            
-                                        </div>
-                                        <div class="col-md-7">
-                                            <div class="form-group mb-3">
-                                                <input type="text" placeholder="First Name" v-model="state.first_name" class="form-control" />
-                                                    <span class="error-msg" v-if="v$.first_name.$error">{{ v$.first_name.$errors[0].$message }} </span>         
-                                            </div>
-
-                                            <div class="form-group mb-3">
-                                                <input type="text" placeholder="Last Name" v-model="state.last_name" class="form-control" />
-                                            <span class="error-msg" v-if="v$.last_name.$error">{{ v$.last_name.$errors[0].$message }} </span>                                            
-                                            </div>
-
-                                            <div class="form-group mb-3">
-                                                <input type="email" placeholder="Email" v-model="state.email" class="form-control" />
-                                                <span class="error-msg" v-if="v$.email.$error">{{ v$.email.$errors[0].$message }} </span>                                           
-                                            </div>     
-
-                                            <div class="form-group">
-                                                <div class="row">
-                                                     <div class="col-12">
-                                                      <vue-tel-input class="form-control" aria-autocomplete="none" v-model="phone" v-on:validate="countryChanged" :inputOptions="options" :dropdownOptions="options2"> </vue-tel-input></div> 
-                                                    <div class="col-12">
-                                                        <span class="error-msg" v-if="v$.phone_number.$error">{{ v$.phone_number.$errors[0].$message }} </span>
-                                                    </div>
+                                    <form>
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <div class="profile-pic">
+                                                    <img :src="picture" class="uploading-image" />
                                                 </div>
-                                                <span class="error-msg"></span>                                            
-                                            </div>                                                                              
+                                                <input type="file" accept="image/png, image/gif, image/jpeg" id="pro-pic" style="display:none" @change=uploadImage>
+                                                <label class="btn btn-primary dark centered" for="pro-pic">
+                                                    Update Profile Picture
+                                                </label>                                            
+                                            </div>
+                                            <div class="col-md-7">
+                                                <div class="form-group mb-3">
+                                                    <input type="text" placeholder="First Name" v-model="state.first_name" class="form-control" />
+                                                        <span class="error-msg" v-if="v$.first_name.$error">{{ v$.first_name.$errors[0].$message }} </span>         
+                                                </div>
+
+                                                <div class="form-group mb-3">
+                                                    <input type="text" placeholder="Last Name" v-model="state.last_name" class="form-control" />
+                                                <span class="error-msg" v-if="v$.last_name.$error">{{ v$.last_name.$errors[0].$message }} </span>                                            
+                                                </div>
+
+                                                <div class="form-group mb-3">
+                                                    <input type="email" placeholder="Email" v-model="state.email" class="form-control" />
+                                                    <span class="error-msg" v-if="v$.email.$error">{{ v$.email.$errors[0].$message }} </span>                                           
+                                                </div>     
+
+                                                <div class="form-group">
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                        <vue-tel-input class="form-control" aria-autocomplete="none" v-model="phone" v-on:validate="countryChanged" :inputOptions="options" :dropdownOptions="options2"> </vue-tel-input></div> 
+                                                        <div class="col-12">
+                                                            <span class="error-msg" v-if="v$.phone_number.$error">{{ v$.phone_number.$errors[0].$message }} </span>
+                                                        </div>
+                                                    </div>
+                                                    <span class="error-msg"></span>                                            
+                                                </div>                                                                              
+                                            </div>
                                         </div>
-                                    </div>
+                                    </form>
                                     <div class="row">
                                         <div class="col-md-12">
                                             <button class="pull-right slide-btn" @click="profilesubmit">Next</button>
@@ -140,16 +142,27 @@
                                     <div class="row">
                                         <div class="col-md-10">
                                             <div class="form-group mb-4">
-                                                <input type="password" placeholder="Password" v-model="state.password" class="form-control" />
-                                            <span class="error-msg" v-if="v$.password.$error">{{ v$.password.$errors[0].$message }} </span>                                            
+                                                <div class="eye-area mb-4">
+                                                    <input v-bind:type="[showPassword ? 'text' : 'password']" placeholder="Password" v-model="state.password" class="form-control" />
+                                                    <div class="eye-box">
+                                                        <i @click="showPassword = !showPassword" :class="[showPassword ? 'ri-eye-off-line' : 'ri-eye-line']" aria-hidden="true"></i>  
+                                                    </div>                                                    
+                                                    <span class="error-msg" v-if="v$.password.$error">{{ v$.password.$errors[0].$message }} </span>     
+                                                </div>                                           
                                             </div>
-                                        </div>                                                                                                                                                                                
+                                        </div>  
+                                                                                                                                                                                                                     
                                     </div>
                                     <div class="row">
                                         <div class="col-md-10">
                                             <div class="form-group mb-4">
-                                                <input type="password" placeholder="Confirm Password" v-model="state.confirm_password" class="form-control" />
-                                            <span class="error-msg" v-if="v$.confirm_password.$error">{{ v$.confirm_password.$errors[0].$message }} </span>                                            
+                                                <div class="eye-area mb-4">
+                                                    <input v-bind:type="[showPasswordConfirm ? 'text' : 'password']" placeholder="Confirm Password" v-model="state.confirm_password" class="form-control" />
+                                                    <div class="eye-box">
+                                                        <i @click="showPasswordConfirm = !showPasswordConfirm" :class="[showPasswordConfirm ? 'ri-eye-off-line' : 'ri-eye-line']" aria-hidden="true"></i>  
+                                                    </div>
+                                                    <span class="error-msg" v-if="v$.confirm_password.$error">{{ v$.confirm_password.$errors[0].$message }} </span>   
+                                                </div>                                         
                                             </div>
                                         </div>                                                                                                                                                                                
                                     </div>
@@ -238,7 +251,7 @@ export default {
             passwordTab: false,
             picture:null,
             countryList: countries, 
-
+            showPasswordConfirm:false,
 
             options: { 
                  placeholder: "Phone Number",
