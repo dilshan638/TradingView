a36bd47dbba5adda62cf2e71c071c52e193a3b37
Kasun Sameera <kasun@persystance.com>
2021-08-11 21:13:33 +0000

    updated UI

--- src/components/Wallet/WalletListTable.vue
+++ src/components/Wallet/WalletListTable.vue
@@ -1,61 +1,84 @@
 <template>
-<div class="card table-card">
-    <div class="card-header">
-        <div class="row">
-            <div class="col-md-9">
-                <h2>Crypto</h2>
-            </div>
-            <div class="col-md-3">
-                <div class="search-form"></div>
+    <div class="card table-card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-9">
+                    <h2>Crypto</h2>
+                </div>
+                <div class="col-md-3">
+                    <div class="search-form">
+                        <input type="text" class="form-control" placeholder="Search" />
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                <table class="table table-hover wallet-table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Coin</th>
+                            <th scope="col" class="text-right">Total</th>
+                            <th scope="col" class="text-right">BTC Values</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src="images/icons/btc@3x.webp" />
+                                <h4>
+                                    BTC
+                                    <span>Bitcoin</span>
+                                </h4>       
+                            </td>
+                            <td class="text-right">0.43434343</td>
+                            <td class="text-right">0.43434343</td>
+                            <td class="action-td">
+                                <a>Deposit</a>
+                                <a>Withdraw</a>
+                                <a>Trade</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img src="images/icons/btc@3x.webp" />
+                                <h4>
+                                    BTC
+                                    <span>Bitcoin</span>
+                                </h4>       
+                            </td>
+                            <td class="text-right">0.43434343</td>
+                            <td class="text-right">0.43434343</td>
+                            <td class="action-td">
+                                <a>Deposit</a>
+                                <a>Withdraw</a>
+                                <a>Trade</a>
+                            </td>
+                        </tr>   
+                        <tr>
+                            <td>
+                                <img src="images/icons/btc@3x.webp" />
+                                <h4>
+                                    BTC
+                                    <span>Bitcoin</span>
+                                </h4>       
+                            </td>
+                            <td class="text-right">0.43434343</td>
+                            <td class="text-right">0.43434343</td>
+                            <td class="action-td">
+                                <a>Deposit</a>
+                                <a>Withdraw</a>
+                                <a>Trade</a>
+                            </td>
+                        </tr>                                            
+                    </tbody>
+                </table>                         
+                </div>
+            </div>
+        </div>                    
     </div>
-    <div class="card-body no-padding no-padding-all">
-        <div class="row">
-            <div class="col-md-12">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Date & Time</th>
-                        <th scope="col">IP Address</th>
-                        <th scope="col">Browser</th>
-                        <th scope="col">Location</th>
-                        <th scope="col">Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>01-07-2021 14.32.22</td>
-                    <td>192.168.0.3</td>
-                    <td>Chrome</td>
-                    <td>London, United Kingdom</td>
-                    <td><span class="success-text">LOGIN</span></td>
-                </tr>
-                <tr>
-                    <td>01-07-2021 14.32.22</td>
-                    <td>192.168.0.3</td>
-                    <td>Chrome</td>
-                    <td>London, United Kingdom</td>
-                    <td><span class="error-text">LOGOUT</span></td>
-                </tr>
-                <tr>
-                    <td>01-07-2021 14.32.22</td>
-                    <td>192.168.0.3</td>
-                    <td>Chrome</td>
-                    <td>London, United Kingdom</td>
-                    <td><span class="error-text">LOGOUT</span></td>
-                </tr>                             
-                </tbody>
-            </table>                         
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <a class="view-more">View More</a>
-            </div>
-        </div>
-    </div>                    
-</div>
 </template>
 
 <script>
@@ -64,6 +87,6 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    @import "../../assets/scss/wallet/wallet";
 </style>
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -10,33 +10,49 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body no-padding no-padding-all">
+                <div class="card-body no-padding no-padding-all table-tab">
                     <div class="row">
                         <div class="col-md-12">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Date</th>
-                                    <th scope="col">Pair</th>
-                                    <th scope="col">Type</th>
-                                    <th scope="col">Side</th>
-                                    <th scope="col">Price</th>
-                                    <th scope="col">Amount</th>
-                                    <th scope="col">Field</th>
-                                    <th scope="col">Total</th>
-                                    <th scope="col">Trigger Conditions</th>
-                                </tr>
-                            </thead>
-                            <tbody>                            
-                            </tbody>
-                        </table>                         
+                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Open Orders</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Order History</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">My Trade History</a>
+                            </li>
+                            </ul>
+                            <div class="tab-content" id="pills-tabContent">
+                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Date</th>
+                                                <th scope="col">Pair</th>
+                                                <th scope="col">Type</th>
+                                                <th scope="col">Side</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Amount</th>
+                                                <th scope="col">Field</th>
+                                                <th scope="col">Total</th>
+                                                <th scope="col">Trigger Conditions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>                            
+                                        </tbody>
+                                    </table>                         
+                                    </div>
+                                </div>                                
+                            </div>
+                            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">2</div>
+                            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">3</div>
+                            </div>                      
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a class="view-more">View More</a>
-                        </div>
-                    </div>
+                    </div>                    
                 </div>                    
             </div>
         </div>
