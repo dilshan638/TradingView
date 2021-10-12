97b5fe95eb3a37fcddff49225a8c01268fcaed49
Kasun Sameera <kasun@persystance.com>
2021-10-12 15:43:24 +0000

    updated dev

--- src/components/Trade/OrderBook.vue
+++ src/components/Trade/OrderBook.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="trade-box">
+      <vue-datepicker-local v-model="time" />
       <div class="trade-header">
         Order Book
         <select class="form-control sel-val" @change="onChange($event)">
@@ -128,7 +129,6 @@
                   {{ parseFloat(sell[0] * sell[1]).toFixed(5) }}
                 </td>
               </tr>
-
               <tr @mouseover="removeoverflow" @mouseleave="resetoverflow"
                 v-for="sell in priceSellBind.slice(0, 11)"
                 :key="sell"
@@ -145,7 +145,6 @@
                 </td>
               </tr>
             </tbody>
-
              <tbody v-else>
               <tr @mouseover="removeoverflow" @mouseleave="resetoverflow"
                 v-for="sell in priceSellBind"
@@ -199,7 +198,6 @@
                   {{ parseFloat(sell[0] * sell[1]).toFixed(5) }}
                 </td>
               </tr>
-
               <tr @mouseover="removeoverflow" @mouseleave="resetoverflow"
                 v-for="sell in priceSellBind"
                 :key="sell"
@@ -248,7 +246,7 @@
         </div>
         <div
           class="trade-body buy-body" 
-          v-if="showbuyandsell == true || showbuy == true"
+          v-if="showbuyandsell == true"
           v-bind:class="[showbuy ? 'buy-full' : 'buy-default']"
         >
           <table lass="table table-hover" style="width: 100% !important" v-if="limitRows">
@@ -484,14 +482,18 @@
 
 <script>
 import axios from "axios";
+import VueDatepickerLocal from 'vue-datepicker-local'
 export default {
   emits: ["sellPriceOrderBookPass", "sellAmountOrderBookPass"],
   name: "orderbook",
-  components: {},
+  components: {
+    VueDatepickerLocal
+  },
   props: ["SelectedSymbol", "pairName"],
 
   data() {
     return {
+      time: new Date(),
       buyselltab: true,
       selltab: false,
       buytab: false,
